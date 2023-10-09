cac6d2ed790c03bdd8fb713fd19018f2aa5abdaa
Alonso Rmz Paez <alonso.rpaez@gmail.com>
2023-10-08 18:27:08 -0600

    Added web design for watch resources

---
+++ app/Models/Post.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    /**
+     * Defining an accessor
+     */
+    public function getExcerptAttribute()
+    {
+        return substr($this->content, 0, 120);
+    }
+
+    /**
+     * Defining an accessor
+     */
+    public function getPublishedAtAttribute()
+    {
+        return $this->created_at->format('d/m/Y');
+    }
+}
