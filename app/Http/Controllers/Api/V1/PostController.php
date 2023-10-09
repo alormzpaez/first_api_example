71651b307675f1610f9f1cbfd864fd50b75e1eac
Alonso Rmz Paez <alonso.rpaez@gmail.com>
2023-10-08 20:58:06 -0600

    Added functionality for show post resource

--- app/Http/Controllers/Api/V1/PostController.php
+++ app/Http/Controllers/Api/V1/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Models\Post;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\V1\PostResource;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -29,7 +30,7 @@ class PostController extends Controller
      */
     public function show(Post $post)
     {
-        return $post;
+        return new PostResource($post);
     }
 
     /**
